cf98e396a1c0f0c10170e02b7c4b4c7f8ad3e25a
Scott Macdonald <scott.scotm@gmail.com>
2023-10-21 08:35:54 +0100

    Support multiple files - and tidy-ups

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,25 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import { Inter } from 'next/font/google';
 
 export const metadata = {
-  title: 'Vercel Blob Starter',
-  description: 'A simple Next.js app with Vercel Blob for image uploads',
-}
+    title: 'Compress Image Files',
+    description: 'A simple Next.js app for uploading and compressing images.',
+};
 
 const inter = Inter({
-  variable: '--font-inter',
-  subsets: ['latin'],
-  display: 'swap',
-})
+    variable: '--font-inter',
+    subsets: ['latin'],
+    display: 'swap',
+});
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.variable}>{children}</body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <body className={inter.variable}>{children}</body>
+        </html>
+    );
 }
