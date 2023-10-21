3d9090b0b37fe754dc1f2a4808bba6b97c161d9d
Scott Macdonald <scott.scotm@gmail.com>
2023-10-21 08:46:14 +0100

    adding pnpm lockfile

--- components/uploader.tsx
+++ components/uploader.tsx
@@ -3,10 +3,29 @@
 import { useState, useMemo, ChangeEvent, FormEvent } from 'react';
 import LoadingDots from './loading-dots';
 
-import toast from 'react-hot-toast';
+import toast, { Toast } from 'react-hot-toast';
 
 import { PutBlobResult } from '@vercel/blob';
 import { Cloud } from './Cloud';
+
+const DismissButton = (t: Toast) => (
+    <button
+        onClick={() => toast.dismiss(t.id)}
+        className="absolute top-0 -right-2 inline-flex text-gray-400 focus:outline-none focus:text-gray-500 rounded-full p-1.5 hover:bg-gray-100 transition ease-in-out duration-150"
+    >
+        <svg className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+            <path
+                fillRule="evenodd"
+                d="M5.293 5.293a1 1 0 011.414 0L10
+        8.586l3.293-3.293a1 1 0 111.414 1.414L11.414
+        10l3.293 3.293a1 1 0 01-1.414 1.414L10
+        11.414l-3.293 3.293a1 1 0 01-1.414-1.414L8.586
+        10 5.293 6.707a1 1 0 010-1.414z"
+                clipRule="evenodd"
+            />
+        </svg>
+    </button>
+);
 
 export default function Uploader() {
     const [files, setFiles] = useState<File[]>([]);
@@ -56,26 +75,7 @@ export default function Uploader() {
                                 </a>
                             </p>
                         </div>
-                        <button
-                            onClick={() => toast.dismiss(t.id)}
-                            className="absolute top-0 -right-2 inline-flex text-gray-400 focus:outline-none focus:text-gray-500 rounded-full p-1.5 hover:bg-gray-100 transition ease-in-out duration-150"
-                        >
-                            <svg
-                                className="h-5 w-5"
-                                viewBox="0 0 20 20"
-                                fill="currentColor"
-                            >
-                                <path
-                                    fillRule="evenodd"
-                                    d="M5.293 5.293a1 1 0 011.414 0L10
-            8.586l3.293-3.293a1 1 0 111.414 1.414L11.414
-            10l3.293 3.293a1 1 0 01-1.414 1.414L10
-            11.414l-3.293 3.293a1 1 0 01-1.414-1.414L8.586
-            10 5.293 6.707a1 1 0 010-1.414z"
-                                    clipRule="evenodd"
-                                />
-                            </svg>
-                        </button>
+                        <DismissButton {...t} />
                     </div>
                 ),
                 { duration: 300000 }
